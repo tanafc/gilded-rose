46d15b367e6b8957f431c520e39b8c8b71dbf20b
Tanausu <tanausu@acidtango.com>
2024-05-02 18:11:29 +0100

    golden: tests made

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,10 +1,22 @@
 import { describe, it, expect } from "vitest"
-import { myFunction } from "./GildedRose.js"
+import { GildedRose, Item } from "./GildedRose.js"
 
-describe("Default test", () => {
-  it("should work", () => {
-    const result = myFunction()
+describe("Gilded Rose", () => {
+  const itemNames = ["Apple", "Sulfuras, Hand of Ragnaros", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert"]
 
-    expect(result).toBe(true)
-  })
+  for (let name of itemNames) {
+    for (let sellIn = 0; sellIn <= 10; sellIn++) {
+      for (let quality = -2; quality <= 51; quality++) {
+        const item = new Item(name, sellIn, quality)
+
+        it("works", () => {
+          const gildedRose = new GildedRose([item])
+
+          const items = gildedRose.updateQuality()
+
+          expect(items[0]).toMatchSnapshot()
+        })
+      }
+    }
+  }
 })
